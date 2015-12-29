a8f9e4fbbe8ebd7389f29ea21ce2916332f20edd
koringz <260713986@qq.com>
2015-12-29 18:24:43 +0800

    up

--- sclientandscreen/js/index.js
+++ sclientandscreen/js/index.js
@@ -13,18 +13,14 @@ function Box(e,t1,t2,rect){
 	this.scry = e.screenY;
 	// 包含浏览器边框的坐标
 
-	this.pgx = e.pageXOffset;
-	this.otwt = e.outerWidth;
-
 	this.clitx = e.clientX;
 	this.clity = e.clientY;
 	// 不包含浏览器的坐标，只显示客户端坐标
-
 
 	t1.value = this.scrx + "," +this.scry;
 	t2.value = this.clitx + "," +this.clity;
 
 	rect.style.display = "block";
-	rect.style.left = this.scrx + "px";
+	rect.style.left = this.clitx + "px";
 	rect.style.top = this.clity + "px";
 }
