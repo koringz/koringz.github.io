8c0f9e81824c84b59fc761df4fb74a29d89b7028
koringz <260713986@qq.com>
2016-01-24 23:06:42 +0800

    up

--- scrollbroadcast/koringz.js
+++ scrollbroadcast/koringz.js
@@ -1,17 +1,45 @@
-function getElementNode(attr_name, element) {
+/* 
+ *
+ * $author koringz
+ * $date 2015-9-27
+ * $version 0.1
+ *
+ * $author koringz
+ * $date 2015-9-29
+ * $version 0.1
+ *
+ */
+function getElementNode(attr_name, node, element) {
 	element = el(element);
-	if(typeof attr_name == "string"){
+	if(isString(attr_name)){
 		if(/^\./g.test(attr_name)){
-			return element.getElementsByClassName(attr_class(attr_name))[0]
+			if(element.getElementsByClassName){
+				return element.getElementsByClassName((new callback(attr_name)).attr_class())[0]
+			}else{
+				var a = [];
+				node = element;
+				if(node.ownerDocument === '[object HTMLDocument]') node.all;
+				for(var i = 0;i<node.length;i++){
+					if(node[i].className == (new callback(attr_name)).attr_class()) a.push(node[i]);
+				}
+				return a
+			}
 		}else if(/^\#/g.test(attr_name)){
-			return element.getElementById(attr_id(attr_name))
+			return element.getElementById((new callback(attr_name)).attr_id())
+		}else{
+			return element.getElementsByTagName(attr_name)[0]
 		}
 	}
-	function attr_class(attr_class){
-		return attr_name.replace(/./,"");
-	}
-	function attr_id(attr_id){
-		return attr_name.replace(/#/,"");
+};
+function callback (element) {
+	reg.apply(this,arguments)
+};
+function reg (options) {
+	this.attr_class = function () {
+		return options.replace(/./,"");
+	};
+	this.attr_id =  function () {
+		return options.replace(/#/,"");
 	}
 };
 function addLister(domnode,eventType,handler){
@@ -26,3 +54,67 @@ function el(element){
 	if(!element){element = document}
 	return element 
 };
+function isString(element){
+	return typeof element === "string"
+};
+function isFunction(element){
+	return typeof element === "function"
+};
+function isNumber(element){
+	return typeof element === "number"
+};
+function isObject(element){
+	return typeof element === "object"
+};
+function isArray(element){
+	return typeof element === "array"
+};
+function isBoolean(element){
+	return typeof element === "boolean"
+};
+
+
+
+function arr(argument) {
+	this.createarr = []
+}
+arr.prototype =  {
+	method_name:function (argument) {
+		if(this.createarr.length == 0){
+			this.eventmap = this.enresized.bindAsEventListener(as);	
+		}
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
